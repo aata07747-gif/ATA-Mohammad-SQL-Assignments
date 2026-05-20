15337567dec37417e4426575b00a20ba11ec116a
aata07747-gif <aata07747@gmail.com>
2026-05-20 16:59:58 +0430

    Create sum_1_to_100.sql

---
+++ sum_1_to_100.sql
@@ -0,0 +1,12 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+    total NUMBER := 0;
+BEGIN
+    FOR i IN 1..100 LOOP
+        total := total + i;
+    END LOOP;
+
+    DBMS_OUTPUT.PUT_LINE('Sum = ' || total);
+END;
+/
